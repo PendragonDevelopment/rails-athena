9161920ecfd060e1c038676a770836b6ee314866
Jordan Burke <me@jordankburke.com>
2014-02-18 20:08:12 -0500

    Updated users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:the_team]
 
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user], :as => :admin)
+    if @user.update_attributes(user_params, :as => :admin)
       redirect_to users_path, :notice => "User updated."
     else
       redirect_to users_path, :alert => "Unable to update user."
